dfb7bba3bec892cb00e2e7c06253ce003773792a
Irina-Lursmanashvili <Irina-Lursmanashvili>
2023-09-04 21:00:04 +0400

    LastHomeWork.

--- src/main/java/stepObject/RegistrationPageSteps.java
+++ src/main/java/stepObject/RegistrationPageSteps.java
@@ -9,47 +9,55 @@ public class RegistrationPageSteps extends RegistrationPage {
         authorizationButton.click();
         return this;
     }
+
     @Step("სახელის ველის შევსება მნიშვნელობით: {Name}")
     public RegistrationPageSteps fillName(String Name) {
         nameInPut.setValue(Name);
         return this;
     }
+
     @Step("გვარის ველის შევსება მნიშვნელობით: {lastName}")
     public RegistrationPageSteps fillSurname(String lastName) {
         surnameInPut.setValue(lastName);
         return this;
     }
+
     @Step("დაბადების თარიღის არჩევა მნიშვნელობით: {BirthDate}")
     public RegistrationPageSteps fillBirthDate(String BirthDate) {
         birthDateInPut.setValue(BirthDate);
         return this;
-        ////$("#datepicker").selectOption(15/07/1995); ამას სად ვწერთ?..........................
     }
+
     @Step("ელ.ფოსტის ველის შევსება მნიშვნელობით: {Email}")
     public RegistrationPageSteps fillEmail(String Email) {
         emailInPut.setValue(Email);
         return this;
     }
+
     @Step("ტელეფონის ნომრის ველის შევსება მნიშვნელობით: {PhoneNumber}")
     public RegistrationPageSteps fillPhoneNumber(String PhoneNumber) {
         phoneNumberInPut.setValue(PhoneNumber);
         return this;
     }
+
     @Step("პაროლის ველის შევსება მნიშვნელობით: {Password}")
     public RegistrationPageSteps fillPassword(String Password) {
         passwordInput.setValue(Password);
         return this;
     }
+
     @Step("პაროლის დადასტურება მნიშვნელობით: {ConfirmPassword}")
     public RegistrationPageSteps fillConfirmPassword(String ConfirmPassword) {
         confirmPasswordInput.setValue(ConfirmPassword);
         return this;
     }
+
     @Step("ვეთანხმები წესებსა და პირობებს")
     public RegistrationPageSteps fillAgreeTerms() {
         AgreeTermsInPut.pressEnter();
         return this;
     }
+
     @Step("რეგისტრაციის ღილაკზე დაჭერა")
     public RegistrationPageSteps ClickRegistrationButton() {
         registrationButton.click();
